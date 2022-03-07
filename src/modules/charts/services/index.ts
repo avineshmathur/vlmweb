73ca6db5ae411b784a534922c26cb61790248fd3
avinesh <avineshmathur@gmail.com>
2022-03-07 13:40:54 +0530

    list register api1

--- src/modules/charts/services/index.ts
+++ src/modules/charts/services/index.ts
@@ -7,7 +7,7 @@ export * from './charts.service';
 export * from './register.service';
 export const Config = {
 	//API_BASE_PATH : 'https://jsonplaceholder.typicode.com/users/',
-	API_BASE_PATH : 'http://10.181.177.196/:3000/',
+	API_BASE_PATH : 'http://10.181.177.196:3000/',
     TIMEOUT_SECONDS: 3000000000000,
     API_OTHERS_KEY : "aHC$kykHa=aZHC#yZZkA$#HyHaCkH=k#akHkkZ$C$ZaAakHZ",
     SUBDOMAIN: "",
